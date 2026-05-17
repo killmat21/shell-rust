a1f8649dacfeb36cd8fcb06ce1ad37d457b3fb72
Killian Mathieu <ant666@Mac-001.lan>
2026-05-17 20:01:02 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> io::Result<()> {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buffer: String = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    let command: &str = buffer.split_whitespace().next().unwrap();
+    let error: String = String::from(command) + ": command not found\n";
+    io::stderr().write_all(&error.as_bytes())?;
+    Ok(())
 }
