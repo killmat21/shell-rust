1d21b9ca0e86a269b861a0821825ff0e28796ae3
Killian Mathieu <ant666@Mac-001.lan>
2026-05-17 20:03:16 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut buffer: String = String::new();
-    io::stdin().read_line(&mut buffer)?;
+        let mut buffer: String = String::new();
+        io::stdin().read_line(&mut buffer)?;
 
-    let command: &str = buffer.split_whitespace().next().unwrap();
-    let error: String = String::from(command) + ": command not found\n";
-    io::stderr().write_all(&error.as_bytes())?;
+        let command: &str = buffer.split_whitespace().next().unwrap();
+        let error: String = String::from(command) + ": command not found\n";
+        io::stderr().write_all(&error.as_bytes())?;
+    }
     Ok(())
 }
