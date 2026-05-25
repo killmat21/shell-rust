72a442421b2f6db6442121e8af8f2bcf45a74a57
Killian Mathieu <ant666@MacBook-Pro-de-Killian.local>
2026-05-25 19:09:51 +0200

    (IMPROVEMENT): Changed the buffer snaitizing to handle quotes and double quotes behavior correctly [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
-use std::{env, fs, io::{self, ErrorKind, Write}, os::unix::fs::PermissionsExt, path::Path, process::Command};
+use std::{env, fs, io::{self, ErrorKind, Write}, os::unix::fs::PermissionsExt, path::Path, process::Command, thread::current};
 use io::Error;
 use itertools::join;
 use aho_corasick::AhoCorasick;
+mod utils;
 
-const ALLOWED_COMMANDS: [&str; 5] = ["exit", "echo", "type", "pwd", "cd"];
+const BUILTIN_COMMANDS: [&str; 5] = ["exit", "echo", "type", "pwd", "cd"];
 
-fn _sanitize_user_input(buffer: &String) -> String {
+fn _sanitize_buffer(buffer: &String) -> (String, Vec<String>) {
+    let mut command_with_args: Vec<String> = Vec::new();
+    let mut in_quotes: bool = false;
+    let mut in_double_quotes: bool = false;
+    let mut current_quote: String = String::from("");
+    for c in buffer.chars() {
+        if c == '\"' && !in_quotes {
+            in_double_quotes = !in_double_quotes;
+        }
+        else if c == '\'' && !in_double_quotes {
+            in_quotes = !in_quotes;
+        }
+        else if in_double_quotes == false && in_quotes == false && c.is_whitespace() {
+            if !current_quote.is_empty() {
+                command_with_args.push(current_quote);
+                current_quote = String::from("");
+            }
+        }
+        else {
+            current_quote.push(c);
+        }
+    }
+
+    let command: String = match command_with_args.first() {
+        Some(value) => {
+            let command_value = value.to_string();
+            command_with_args.remove(0);
+            command_value
+        },
+        None => String::from(""),
+    };
+    (command, command_with_args)
+}
+
+fn _sanitize_command(buffer: &String) -> String {
     let patterns: &[&str; 2] = &["\"", "\'"];
     let replace_with: &[&str; 2] = &["", ""];
 
@@ -13,7 +48,8 @@ fn _sanitize_user_input(buffer: &String) -> String {
     ac.replace_all(buffer, replace_with)
 }
 
-fn _find_executable_command_in_path(command: &str) -> Result<String, Error> {
+
+fn _find_executable_command_in_path(command: &String) -> Result<String, Error> {
     let path_env_var: String = env::var("PATH").unwrap();
     let paths = path_env_var.split(":");
     for path in paths {
@@ -23,7 +59,7 @@ fn _find_executable_command_in_path(command: &str) -> Result<String, Error> {
         };
         for entry in entries.into_iter() {
             let path = entry.unwrap().path();
-            if path.is_file() && path.file_name().unwrap() == command {
+            if path.is_file() && path.file_name().unwrap() == command.as_str() {
                 let utf8_path = path.into_os_string().into_string().unwrap();
                 let md = match fs::metadata(&utf8_path) {
                     Ok(metadata) => metadata,
@@ -39,7 +75,7 @@ fn _find_executable_command_in_path(command: &str) -> Result<String, Error> {
     Err(Error::new(ErrorKind::NotFound, "No executable found in PATH environment variable"))
 }
 
-fn _execute_command(command: &String, args: Vec<&str>) -> Result<(), Error> {
+fn _execute_command(command: &String, args: Vec<String>) -> Result<(), Error> {
     let output = Command::new(command).args(args)
         .output()
         .expect("Failed to execute process");
@@ -64,16 +100,9 @@ fn main() -> io::Result<()> {
 
         let mut buffer: String = String::new();
         io::stdin().read_line(&mut buffer)?;
-        let buffer_sanitize: String = _sanitize_user_input(&buffer);
+        let (command, args) = _sanitize_buffer(&buffer);
 
-        let mut user_input = buffer_sanitize.split_whitespace();
-        let command: &str = match &user_input.next() {
-            Some(val) => val,
-            None => continue
-        };
-        let mut args: Vec<&str> = user_input.collect();
-
-        match command {
+        match command.as_str() {
             "exit" => break,
             "pwd" => {
                 let cwd: String = _get_current_working_dir().expect("");
@@ -85,7 +114,10 @@ fn main() -> io::Result<()> {
                     path = env::home_dir().unwrap().display().to_string();
                 }
                 else {
-                    path = String::from(args[0]);
+                    path = match args.first() {
+                        Some(value) => value.to_string(),
+                        None => String::from(""),
+                    };
                 }
                 let root = Path::new(&path);
                 if env::set_current_dir(&root).is_err() {
@@ -93,16 +125,16 @@ fn main() -> io::Result<()> {
                 }
             },
             "echo" => {
-                let display_str: String = join(&mut args, " ");
+                let display_str: String = join(args, " ");
                 println!("{display_str}");
             },
             "type" => {
                 for arg in args{
-                    if ALLOWED_COMMANDS.contains(&arg) {
+                    if BUILTIN_COMMANDS.contains(&arg.as_str()) {
                         println!("{arg} is a shell builtin");
                     }
                     else {
-                        match _find_executable_command_in_path(arg){
+                        match _find_executable_command_in_path(&arg){
                             Ok(path) => println!("{arg} is {path}"),
                             Err(_) => _print_stderr(format!("{arg}: not found\n")),
                         };
@@ -110,7 +142,7 @@ fn main() -> io::Result<()> {
                 }
             },
             _ => {
-                match _find_executable_command_in_path(command){
+                match _find_executable_command_in_path(&command){
                     Ok(_) => _execute_command(&command.to_owned(), args)?,
                     Err(_) => {
                         _print_stderr(String::from(command) + ": command not found\n");
